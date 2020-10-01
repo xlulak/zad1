85809f2138c073a75738a5091c16a8867a130936
xlulak <xlulak@stuba.sk>
2020-10-01 10:42:05 +0200

    Opravene blikanie LEDky.. stale nejde casovac.. :(

--- Src/main.c
+++ Src/main.c
@@ -72,23 +72,21 @@ int main(void)
 	       *((volatile uint32_t *)((uint32_t)(0x48000000 + 0x0CU))) &= ~(0x3 << 8);
   while (1)
   {
+
+
+
+
+
 	  if(BUTTON_GET_STATE)
 	  {
-		  // 0.25s delay
-		  LL_mDelay(250);
-		  LED_ON;
-		  // 0.25s delay
-		  LL_mDelay(250);
+
 		  LED_OFF;
 	  }
 	  else
 	  {
-		  // 1s delay
-		  LL_mDelay(1000);
+
+
 		  LED_ON;
-		  // 1s delay
-		  LL_mDelay(1000);
-		  LED_OFF;
 	  }
   }
 
